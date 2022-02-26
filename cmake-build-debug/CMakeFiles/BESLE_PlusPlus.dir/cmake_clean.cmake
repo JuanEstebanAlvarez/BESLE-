fe8456fb243a72638307ea0cff4f7d43457113aa
JuanEstebanAlvarez <j.e.alvareznaranjo@utwente.nl>
2022-02-26 15:20:27 +0100

    Adding directories and dealing with cmakelist

---
+++ cmake-build-debug/CMakeFiles/BESLE_PlusPlus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BESLE_PlusPlus"
+  "BESLE_PlusPlus.pdb"
+  "CMakeFiles/BESLE_PlusPlus.dir/Drivers/Users/JuanAlvarez/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BESLE_PlusPlus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
